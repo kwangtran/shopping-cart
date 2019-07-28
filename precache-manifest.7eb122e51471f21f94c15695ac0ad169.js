df18a7bb93e1aee1e16899655f42b321154fcdc4
quangtv13 <quangtv13@fsoft.com.vn>
2019-07-29 00:05:46 +0700

    Updates

---
+++ precache-manifest.7eb122e51471f21f94c15695ac0ad169.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fba03aca5933ee09981f843bdbb41eba",
+    "url": "/shopping-card/index.html"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/shopping-card/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "3ff15eea6921d7602d9d",
+    "url": "/shopping-card/static/js/main.4b98f1ab.chunk.js"
+  },
+  {
+    "revision": "cf56ee20715218196cba",
+    "url": "/shopping-card/static/js/runtime~main.2593f993.js"
+  }
+]);
