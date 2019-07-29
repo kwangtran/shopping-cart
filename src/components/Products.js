307f3c75a7ca3607484f11ad9d70c265ddd48912
quangtv13 <quangtv13@fsoft.com.vn>
2019-07-29 23:03:34 +0700

    053

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react'
 import Product from './Product';
-
-export default class Products extends Component {
+import { connect } from 'react-redux';
+class Products extends Component {
+    showProducts(products) {
+        var result = null;
+        if (products.length > 0) {
+            result = products.map((product, index) => {
+                return (<Product key={index} product={product} />)
+            })
+        }
+        return result;
+    }
     render() {
+        var { products } = this.props;
         return (
             <section className="section">
                 <h1 className="section-heading">Danh Sách Sản Phẩm</h1>
                 <div className="row">
-                    <Product />
-                    <Product />
-                    <Product />
+                    {this.showProducts(products)}
                 </div>
             </section>
 
         )
     }
 }
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        products: state.products
+    }
+}
+export default connect(mapStateToProps, null)(Products);
--- src/reducers/products.js
+++ src/reducers/products.js
@@ -3,26 +3,29 @@ const initialState = [
     {
         id: 1,
         name: 'Iphone',
-        image: '',
+        image: 'https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/H/H0/HH0H2/HH0H2?wid=445&hei=445&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=K7ik72',
         description: 'Sản phẩm do Apple sản xuất',
         price: 500,
-        inventory: 5
+        inventory: 5,
+        rating: 3
     },
     {
         id: 2,
         name: 'SamSung',
-        image: '',
+        image: 'https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/H/H0/HH0H2/HH0H2?wid=445&hei=445&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=K7ik72',
         description: 'Sản phẩm do SamSung sản xuất',
         price: 400,
-        inventory: 2
+        inventory: 2,
+        rating: 4
     },
     {
         id: 3,
         name: 'Sony',
-        image: '',
+        image: 'https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/H/H0/HH0H2/HH0H2?wid=445&hei=445&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=K7ik72',
         description: 'Sản phẩm do Sony sản xuất',
         price: 450,
-        inventory: 5
+        inventory: 5,
+        rating: 5
     }
 ]
 
