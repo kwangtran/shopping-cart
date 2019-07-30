bc1f2699517eddfaf4d7f41d8f09bf1dea4737ba
quangtv13 <quangtv13@fsoft.com.vn>
2019-07-31 03:53:52 +0700

    061

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -2,32 +2,7 @@ import * as actions from '../actions/index'
 import * as Types from '../constants/ActionsTypes'
 var data = JSON.parse(localStorage.getItem('CART'));
 
-const initialState = [
-    {
-        product: {
-            id: 1,
-            name: 'Iphone',
-            image: 'https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/H/H0/HH0H2/HH0H2?wid=445&hei=445&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=K7ik72',
-            description: 'Sản phẩm do Apple sản xuất',
-            price: 500,
-            inventory: 5,
-            rating: 3
-        },
-        quantity: 2
-    },
-    {
-        product: {
-            id: 2,
-            name: 'SamSung',
-            image: 'https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/H/H0/HH0H2/HH0H2?wid=445&hei=445&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=K7ik72',
-            description: 'Sản phẩm do SamSung sản xuất',
-            price: 400,
-            inventory: 2,
-            rating: 4
-        },
-        quantity: 21
-    }
-]
+const initialState = data ? data : []
 
 const cart = (state = initialState, action) => {
     var { product, quantity } = action;
@@ -35,7 +10,6 @@ const cart = (state = initialState, action) => {
     switch (action.type) {
         case Types.ADD_TO_CART:
             var index = findProductInCart(state, product);
-            console.log(index)
             if (index !== -1) {
                 state[index].quantity += quantity;
             } else {
@@ -44,6 +18,7 @@ const cart = (state = initialState, action) => {
                     quantity
                 })
             }
+            localStorage.setItem('CART',JSON.stringify(state));
             return [...state]
         default:
             return [...state];
